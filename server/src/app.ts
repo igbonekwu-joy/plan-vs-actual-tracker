9f1029f02847d373e80bc0c91231ab41c1ab7de8
Joy <joykenneth605@gmail.com>
2026-09-03 14:55:12 +0100

    feat(auth): add crsf attachement to routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import type { Application } from 'express';
+import cookieParser from 'cookie-parser';
 import authRoutes from './modules/auth/auth.routes';
 import { authGuard } from './middleware/authGuard';
 import { errorHandler } from './middleware/errorHandler';
+import { attachCSRF } from './middleware/csrfHandler';
 
 const app: Application = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(attachCSRF);
 app.use('/api/auth', authRoutes);
 app.get('/api/protected', authGuard, (req, res) => {
 	res.status(200).json({ userId: req.userId });
